fbb425a6808df9d075e7264f30abd8011dd866ee
Kri <kri.boats@gmail.com>
2025-01-21 23:03:28 +0100

    changed level endings added pan function to camera reworked scoreboard made death_zone into a tool script so its CollisionShapes will always be unique

--- objects/death_zone.gd
+++ objects/death_zone.gd
@@ -1,5 +1,12 @@
+@tool
 class_name DeathZone
 extends Area2D
+
+@onready var shape: CollisionShape2D = $CollisionShape2D
+
+
+func _ready():
+	shape.shape = shape.shape.duplicate()
 
 
 func _on_body_entered(body):
---
+++ objects/level_end.gd
@@ -0,0 +1,39 @@
+class_name LevelEnd
+extends Area2D
+
+@onready var guy: Area2D = $Guy
+@onready var guy_sprite: Sprite2D = $Guy/Sprite2D
+
+var locked_in: bool = false
+const LOCK_IN_FORCE: float = 5000
+
+var wait_timer: Timer
+const WAIT_TIME: float = 2
+
+
+func _ready():
+	wait_timer = Timer.new()
+	add_child(wait_timer)
+
+
+func _physics_process(delta):
+	if locked_in:
+		var dir: Vector2 = guy.global_position - GameState.boulder.global_position
+		GameState.boulder.apply_central_force(dir * LOCK_IN_FORCE * delta)
+
+
+func _on_body_entered(body):
+	if body is Boulder:
+		GameState.camera.set_follow_target(guy_sprite)
+		locked_in = true
+
+
+func _on_guy_body_entered(body):
+	if body is Boulder:
+		body.disable_input()
+		guy.hide()
+		locked_in = false
+		wait_timer.start(WAIT_TIME)
+		await wait_timer.timeout
+		
+		# TODO: Show level end screen
